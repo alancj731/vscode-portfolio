f13c06775eb5b95bffe611cdac2aca923b365413
alancj731 <winnipegdatafan@gmail.com>
2025-12-14 00:02:11 -0600

    fix pics

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  basePath: '/vscode-portfolio',
   images: {
     remotePatterns: [
       { hostname: 'res.cloudinary.com', protocol: 'https' },
